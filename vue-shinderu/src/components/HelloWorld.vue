424d5ca4bdf8d1baf7ca9f2a313417d5b20f1e69
z <z@zs-MacBook-Pro.local>
2024-11-23 23:50:11 -0500

    firebase hosting and github actions setup

--- vue-shinderu/src/components/HelloWorld.vue
+++ vue-shinderu/src/components/HelloWorld.vue
@@ -6,8 +6,8 @@ defineProps({
   msg: String,
 })
 
-const count = ref(0)
-let channelName = ref("unknown")
+const count = ref(0);
+let channelName = ref("unknown");
 
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 // Will eventually store the authenticated user's access_token
@@ -78,14 +78,6 @@ async function setupDiscordSdk() {
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
 
   <h1>{{ channelName }}</h1>
 </template>
